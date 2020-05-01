56137dfcffafd3571c38449835153de70e70f786
jarviscm <chris@reactivelions.com>
2020-05-01 07:11:04 -0600

    connect upload care and remove yellowcake references

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'dudeidk',
+    siteUrl: 'https://dreamy-perlman-a67bac.netlify.app'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'dudeidk',
+        short_name: 'dudeidk',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
